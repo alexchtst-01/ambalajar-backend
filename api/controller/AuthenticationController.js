d2a3b2928de596093e57c142d7a1258a157bd9b5
alexchtst-01 <acinatra@gmail.com>
2024-12-16 14:22:14 +0700

    create authentication controller login, getme, and logout

--- api/controller/AuthenticationController.js
+++ api/controller/AuthenticationController.js
@@ -1,5 +1,119 @@
-export const login = async (req, res) => {}
+import User from "../models/sql/User.js";
+import { compare } from "bcrypt";
 
-export const me = async (req, res) => {}
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-export const logout = async (req, res) => {}
+    // email dan password validation field
+    if (!email || !password) {
+      return res.status(400).json({ msg: "Email dan password harus diisi" });
+    }
+
+    // existing user by email
+    const existuser = await User.findOne({
+      where: { email: email },
+    });
+    if (!existuser) {
+      return res.status(404).json({
+        msg: "user tidak ditemukan",
+        data: {
+          nonExistingEmail: email,
+        },
+      });
+    }
+
+    // match the password
+    const isMatch = await compare(password, existuser.password);
+    if (!isMatch) {
+      return res.status(403).json({
+        msg: "password yang anda masukan salah",
+        data: {
+          wrongPassword: password,
+        },
+      });
+    }
+
+    // set session
+    req.session.userId = existuser.userId;
+    req.session.role = existuser.role;
+
+    return res.status(200).json({
+      msg: "successfully login",
+      data: {
+        userId:
+          existuser.userId /* kalo di development perlu kalo di production ga perlu */,
+        email: existuser.email,
+        role: existuser.role,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      msg: `terjadi kesalahan ${error.message}`,
+      data: {
+        err: error,
+      },
+    });
+  }
+};
+
+export const me = async (req, res) => {
+  try {
+    if (!req.session.userId) {
+      return res.status(400).json({
+        msg: "anda tidak login",
+        data: {
+          getmeInfo: "session tidak ditemukan",
+        },
+      });
+    }
+    return res.status(200).json({
+      msg: "anda sudah terauthentikasi dan login",
+      data: {
+        userId: req.session.userId,
+        role: req.session.role,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      msg: `terjadi kesalahan ${error.message}`,
+      data: {
+        err: error,
+      },
+    });
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    if (!req.session.userId) {
+      return res.status(400).json({
+        msg: "anda tidak login",
+        data: {
+          logoutInfo: "session tidak ditemukan",
+        },
+      });
+    }
+    req.session.destroy((error) => {
+      if (error) {
+        return res.status(400).json({
+          msg: `terjadi kesalahan logout ${error.message}`,
+          data: {
+            logoutInfo: error,
+          },
+        });
+      }
+      return res.status(200).json({
+        msg: "anda berhasil logout",
+        data: {},
+      });
+    });
+  } catch (error) {
+    res.status(500).json({
+      msg: `terjadi kesalahan ${error.message}`,
+      data: {
+        err: error,
+      },
+    });
+  }
+};
